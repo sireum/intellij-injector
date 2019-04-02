a63f996b0aab83e3b978d7574697da4dc5e2b13d
Robby <robby@k-state.edu>
2019-04-02 17:58:34 -0500

    Revised versioning.

--- post.sh
+++ post.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-VER=$(git log -n 1 --date=format:%Y%m%d.%H%M --pretty=format:4.%cd.%h)
+VER=$(git log -n 1 --date=format:%Y%m%d --pretty=format:4.%cd.%h)
 7z x sireum-injector.jar META-INF/plugin.xml
 sed -i.bak "s/4.0.0-SNAPSHOT/${VER}/g" META-INF/plugin.xml
 7z a sireum-injector.jar META-INF/plugin.xml

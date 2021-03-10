759034e392113fc32df9d026c206a5c096e0e5de
Robby <robby@santoslab.org>
2021-03-10 14:36:36 -0600

    Updated post.sh.

--- post.sh
+++ post.sh
@@ -4,5 +4,5 @@ VER=$(git log -n 1 --date=format:%Y%m%d --pretty=format:4.%cd.%h)
 sed -i.bak "s/5.0.0-SNAPSHOT/${VER}/g" META-INF/plugin.xml
 7z a sireum-injector.jar META-INF/plugin.xml
 rm -fR META-INF
-mv sireum-injector.jar sireum-injector.zip
+mv sireum-injector.jar intellij-injector.zip
 echo $VER
